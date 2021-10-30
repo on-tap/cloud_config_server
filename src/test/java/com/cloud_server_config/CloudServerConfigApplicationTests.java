9f447c714e07422cfc4a59c97b8dad5ea9fe958f
phuoc1707 <tdphuoc77@gmail.com>
2021-10-30 23:06:54 +0700

    cloud config server

---
+++ src/test/java/com/cloud_server_config/CloudServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_server_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
